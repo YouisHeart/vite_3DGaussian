f03899a64a989f802165085ed34a04430da8c83d
Alex <2422892983@qq.com>
2026-03-16 18:08:20 +0800

    inital commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -64,24 +64,37 @@ async function init() {
   tilesRenderer.setCamera(camera)
   tilesRenderer.setResolutionFromRenderer(camera, renderer)
 
+  // 更新矩阵并设置相机位置
+  let loadedTileSetHandled = false
   // tiles 加载完成
   tilesRenderer.addEventListener("load-root-tileset", () => {
+    if (loadedTileSetHandled) return;
+    loadedTileSetHandled = true;
 
-    const sphere = new THREE.Sphere()
-    tilesRenderer.getBoundingSphere(sphere)
+    const sphere = new THREE.Sphere();
+    tilesRenderer.getBoundingSphere(sphere);
+    const center = sphere.center.clone(); // 获取包围球中心
+    const radius = sphere.radius; // 获取包围球半径
+    controls.target.copy(center); // 把控制器目标设为包围球中心 
+    const offset = new THREE.Vector3(radius * 2, radius, 0); // 给相机一个偏移
+    camera.position.copy(center).add(offset); // 设置相机位置
 
-    // 模型移动到原点
-    tilesRenderer.group.position
-      .copy(sphere.center)
-      .multiplyScalar(-1)
+    const m = (tilesRenderer).root.transform; // 获取原始矩阵
+    const rotationMat3 = new THREE.Matrix3().set(m[0], m[1], m[2], m[4], m[5], m[6], m[8], m[9], m[10]); // 取出旋转部分
+    rotationMat3.transpose(); // 逆旋转
+    const rotationMat4 = new THREE.Matrix4().setFromMatrix3(rotationMat3); // 转回Matrix4以便应用
+    const rotX90 = new THREE.Matrix4().makeRotationX((90 * Math.PI) / 180); // x轴旋转90度矩阵
+    rotationMat4.multiply(rotX90); // 合并矩阵（由z轴向上坐标系 转为 y轴向上坐标系）
+    const translationMatrix1 = new THREE.Matrix4().makeTranslation(center.x, center.y, center.z); // T(center)
+    const translationMatrix2 = new THREE.Matrix4().makeTranslation(-center.x, -center.y, -center.z); // T(-center)
+    const finalMatrix = new THREE.Matrix4().multiplyMatrices(translationMatrix1, rotationMat4).multiply(translationMatrix2); // 最终矩阵 = T(center) * R⁻¹ * T(-center)
 
-    // 控制器中心点
-    controls.target.copy(new THREE.Vector3(0,0,0))
-    controls.update()
+    tilesRenderer.group.matrix.copy(finalMatrix); // 设置矩阵
+    tilesRenderer.group.matrixAutoUpdate = false; // 禁止自动更新矩阵
+    tilesRenderer.group.updateMatrixWorld(true); // 更新矩阵
   })
 
   scene.add(tilesRenderer.group)
-
   animate()
 }
 
